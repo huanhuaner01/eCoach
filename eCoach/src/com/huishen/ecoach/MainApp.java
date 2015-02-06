e38d66df60eda9aa508c9fcb3e7d20e4ab9b358c
Muyangmin <muyangmin@foxmail.com>
2015-02-06 14:28:49 +0800

    引入了Volley库。

--- eCoach/src/com/huishen/ecoach/MainApp.java
+++ eCoach/src/com/huishen/ecoach/MainApp.java
@@ -1,5 +1,9 @@
 package com.huishen.ecoach;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 import android.app.Application;
 
 /**
@@ -9,4 +13,33 @@ import android.app.Application;
  */
 public final class MainApp extends Application {
 	
+	private static MainApp instance;
+	private RequestQueue requestQueue;
+	
+	//Singleton
+	public static MainApp getInstance() {
+		return instance;
+	}
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		instance = this; //this first (and the only) instance created by system.
+		init();
+	}
+	/**
+	 * 初始化各种全局变量和第三方库。	
+	 */
+	private void init(){
+		requestQueue = Volley.newRequestQueue(this);
+	}
+
+	/**
+	 * 提交新的网络请求。
+	 */
+	public final <T> void addNetworkRequest(Request<T> request){
+		if (requestQueue != null){
+			requestQueue.add(request);
+		}
+	}
 }
