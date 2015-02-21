5273a065d0a7c78a02583b177a709cf0ccb9a917
Muyangmin <muyangmin@foxmail.com>
2015-02-21 21:59:05 +0800

    完成了订单状态界面的UI绘制（合并了“已取消”和“已评价”两种状态）。

---
+++ eCoach/src/com/huishen/ecoach/ui/order/OrderStateActivity.java
@@ -0,0 +1,15 @@
+package com.huishen.ecoach.ui.order;
+
+import com.huishen.ecoach.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OrderStateActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_order_state);
+	}
+}
