7037f1aed6de01b9da676692f14c4a7bd9662167
Muyangmin <muyangmin@foxmail.com>
2015-03-07 10:23:32 +0800

    加入了招生管理页面。

---
+++ eCoach/src/com/huishen/ecoach/ui/recruit/RecruitActivity.java
@@ -0,0 +1,14 @@
+package com.huishen.ecoach.ui.recruit;
+
+import com.huishen.ecoach.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RecruitActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_recruit);
+	}
+}
